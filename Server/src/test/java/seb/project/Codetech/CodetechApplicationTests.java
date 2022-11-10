fa0a4d1f6e8fcc0856528b8082925a4b7eb59afc
NewWorld_ <mf_cheonwol@naver.com>
2022-11-10 15:15:08 +0900

    chore: 누락 파일 추가

---
+++ Server/src/test/java/seb/project/Codetech/CodetechApplicationTests.java
@@ -0,0 +1,13 @@
+package seb.project.Codetech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodetechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
