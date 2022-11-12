ae9d35cda286679ba00a29e6c727d8cc654fc2be
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2022-11-12 22:03:29 +0900

    build: 프론트 CI/CD 테스트

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,29 @@
-// import React from "react";
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`https://codetech.nworld.dev/codetech`)
+      .then((res) => {
+        if (!res.ok) {
+          // error coming back from server
+          throw Error('could not fetch the data for that resource');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        console.error(err.message);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>{data}</p>
         <a></a>
       </header>
     </div>
