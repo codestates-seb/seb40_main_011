b04ee579956d4cc3f07803ae0668f8abccdeadd6
toneofrain <toneofrain@gmail.com>
2022-11-12 21:59:36 +0900

    test: 테스트용 컨트롤러 작성 (#5)

---
+++ server/src/main/java/seb/project/Codetech/HelloController.java
@@ -0,0 +1,15 @@
+package seb.project.Codetech;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class HelloController {
+
+	@GetMapping("/codetech")
+	public String hello() {
+		return "codeTech";
+	}
+}
