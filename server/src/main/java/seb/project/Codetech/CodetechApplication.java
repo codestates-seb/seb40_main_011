91dba52d6b33d997ea22bb8973f6684fa6d189f6
toneofrain <toneofrain@gmail.com>
2022-11-14 11:08:44 +0900

    feat: @EnableJpaAuditing 추가 (#11)
    엔티티의 생성, 수정일자를 관리하기 위해 메인 클래스에 @EnableJpaAuditing 추가

--- server/src/main/java/seb/project/Codetech/CodetechApplication.java
+++ server/src/main/java/seb/project/Codetech/CodetechApplication.java
@@ -2,8 +2,10 @@ package seb.project.Codetech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CodetechApplication {
 
 	public static void main(String[] args) {
