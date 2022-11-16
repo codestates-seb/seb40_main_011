64223bf4057e36a0ec1050236e8fa3e88d299600
toneofrain <toneofrain@gmail.com>
2022-11-16 18:57:45 +0900

    feat: SnackReview 컨트롤러, 서비스, 리포지토리 계층간 연동 (#68)

--- server/src/main/java/seb/project/Codetech/snackreview/controller/SnackReviewControllerV1.java
+++ server/src/main/java/seb/project/Codetech/snackreview/controller/SnackReviewControllerV1.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.Positive;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,30 +18,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 import seb.project.Codetech.snackreview.dto.SnackReviewControllerDto;
+import seb.project.Codetech.snackreview.dto.SnackReviewServiceDto;
+import seb.project.Codetech.snackreview.mapper.SnackReviewControllerMapper;
+import seb.project.Codetech.snackreview.service.SnackReviewService;
 
 @RestController
 @RequestMapping("/api/v1/categories/snackreviews")
 @RequiredArgsConstructor
 @Validated
 public class SnackReviewControllerV1 {
+	private final SnackReviewService snackReviewService;
+	private final SnackReviewControllerMapper dtoMapper;
+
 	@GetMapping
 	public ResponseEntity getSnackReview() {
 		return ResponseEntity.ok().build();
 	}
 
 	@PostMapping
-	public ResponseEntity postSnackReview(@Valid @RequestBody SnackReviewControllerDto.Post request) {
-		return ResponseEntity.status(HttpStatus.CREATED).build();
+	public ResponseEntity postSnackReview(
+		@AuthenticationPrincipal String loginEmail,
+		@Valid @RequestBody SnackReviewControllerDto.Post request
+	) {
+		SnackReviewServiceDto.Create createDto = dtoMapper.postDtoToCreateDto(loginEmail, request);
+		Long createdId = snackReviewService.createSnackReview(createDto);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdId);
 	}
 
 	@PatchMapping("/{id}")
-	public ResponseEntity patchSnackReview(@Positive @PathVariable Long id,
-		@Valid @RequestBody SnackReviewControllerDto.Patch request) {
-		return ResponseEntity.ok().build();
+	public ResponseEntity patchSnackReview(
+		@Positive @PathVariable Long id,
+		@Valid @RequestBody SnackReviewControllerDto.Patch request
+	) {
+		SnackReviewServiceDto.Update updateDto = dtoMapper.patchDtoToUpdateDto(id, request);
+		Long updatedId = snackReviewService.updateSnackReview(updateDto);
+
+		return ResponseEntity.ok().body(updatedId);
 	}
 
 	@DeleteMapping("/{id}")
 	public ResponseEntity deleteSnackReview(@Positive @PathVariable Long id) {
+		snackReviewService.deleteSnackReview(id);
+
 		return ResponseEntity.noContent().build();
 	}
 }
