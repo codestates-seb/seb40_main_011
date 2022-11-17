a563bf532931680b93bfce1eda55c4022106e2b1
toneofrain <toneofrain@gmail.com>
2022-11-17 14:49:12 +0900

    feat: 요청 파라미터 DTO 생성 (#79)

--- server/src/main/java/seb/project/Codetech/snackreview/dto/SnackReviewControllerDto.java
+++ server/src/main/java/seb/project/Codetech/snackreview/dto/SnackReviewControllerDto.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.Positive;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import seb.project.Codetech.product.entity.Type;
 import seb.project.Codetech.snackreview.entity.SnackReview;
 
@@ -30,5 +31,26 @@ public class SnackReviewControllerDto {
 		private SnackReview.Score score;
 		@NotBlank
 		private String content;
+	}
+
+	@Getter
+	@Setter
+	@NoArgsConstructor
+	public static class GetFirst {
+		@Positive
+		public Long productId;
+		public int fistSize;
+	}
+
+	@Getter
+	@NoArgsConstructor
+	public static class GetMore {
+		@Positive
+		private Long productId;
+		private int firstSize;
+		private int page;
+		private int size;
+		private String sort;
+		private String order;
 	}
 }
