675821452bf73fa59bc7c1bad1cbf1ff4514d0e5
toneofrain <toneofrain@gmail.com>
2022-11-17 15:18:45 +0900

    feat: 응답 DTO 생성 (#79)

---
+++ server/src/main/java/seb/project/Codetech/snackreview/dto/SnackReviewResponseDto.java
@@ -0,0 +1,39 @@
+package seb.project.Codetech.snackreview.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+public class SnackReviewResponseDto {
+	@Getter
+	@NoArgsConstructor
+	public static class First {
+		private long total;
+		private float avgCe;
+		private float avgQlt;
+		private float avgStf;
+		private float avgDsn;
+		private float avgPerf;
+		private boolean hasNext;
+		private List<Card> cards;
+	}
+
+	@Getter
+	@NoArgsConstructor
+	public static class More {
+		private boolean hasNext;
+		private List<Card> cards;
+	}
+
+	public static class Card {
+		private String nickName;
+		private String image;
+		private int costEfficiency;
+		private int quality;
+		private int satisfaction;
+		private int design;
+		private int performance;
+		private String content;
+	}
+}
