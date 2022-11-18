d1d17f816be60a04fb8aba51a372378087b0c234
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2022-11-18 17:55:23 +0900

    fix: 마이페이지 모달 타입 수정

--- client/src/components/Modal/EditProfileImg.tsx
+++ client/src/components/Modal/EditProfileImg.tsx
@@ -1,8 +1,15 @@
 //프로필 이미지 변경
-import { useState } from 'react';
+import { useState, SetStateAction } from 'react';
 import { BsXLg } from 'react-icons/bs';
+import { EditProfileImg } from '../MyPage/Profile';
+import { AxiosPromise } from 'axios';
 
-const EditProgileImg = () => {
+const EditProgileImg = ({
+  isEditProfileImg,
+  setIsEditProfileImg,
+}: EditProfileImg) => {
+  console.log(isEditProfileImg);
+
   return (
     <div className="modal-bg">
       <div className="modal-window h-[700px]">
