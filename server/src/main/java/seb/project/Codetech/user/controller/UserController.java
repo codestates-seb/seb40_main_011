71b6e500573dee807947e40296d5ae52a0cb7f3b
kwang <hea0408never@naver.com>
2022-11-20 14:56:01 +0900

    fix: 토큰 리스폰스 형태 수정, logout static 수정, 컨트롤러 제네릭 타입 수정 (#66)

--- server/src/main/java/seb/project/Codetech/user/controller/UserController.java
+++ server/src/main/java/seb/project/Codetech/user/controller/UserController.java
@@ -1,13 +1,13 @@
 package seb.project.Codetech.user.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import seb.project.Codetech.user.dto.UserPatchDto;
 import seb.project.Codetech.user.dto.UserPostDto;
+import seb.project.Codetech.user.dto.UserResponseDto;
 import seb.project.Codetech.user.dto.UserWithdrawDto;
 import seb.project.Codetech.user.entity.User;
 import seb.project.Codetech.user.mapper.UserMapper;
@@ -30,21 +30,21 @@ public class UserController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity registerUser(@Valid @RequestBody UserPostDto register){
+    public ResponseEntity<User> registerUser(@Valid @RequestBody UserPostDto register){
         User user = mapper.userRegisterToUser(register);
         User registerUser = userService.registerUser(user);
         return ResponseEntity.ok(registerUser);
     }
 
     @PatchMapping("/user")
-    public ResponseEntity patchUser(@AuthenticationPrincipal String email,
-                                    @Valid @RequestBody UserPatchDto patch){
+    public ResponseEntity<UserResponseDto> patchUser(@AuthenticationPrincipal String email,
+                                                     @Valid @RequestBody UserPatchDto patch){
         User user = userService.updateUser(email,mapper.userPatchDtoToUser(patch));
         return ResponseEntity.ok(mapper.userToUserResponseDto(user));
     }
 
     @GetMapping("/user")
-    public ResponseEntity getUser(@AuthenticationPrincipal String email){
+    public ResponseEntity<UserResponseDto> getUser(@AuthenticationPrincipal String email){
         User user = userService.findUser(email);
         return ResponseEntity.ok(mapper.userToUserResponseDto(user));
     }
@@ -54,11 +54,11 @@ public class UserController {
                                        @Valid @RequestBody UserWithdrawDto withdraw,
                                        HttpServletRequest request){
         User user = userService.withdrawUser(email,mapper.userWithdrawDtoToUser(withdraw));
-        UserService.logout(request);
+        userService.logout(request);
     }
 
     @GetMapping("/logout")
     public void logoutUser(HttpServletRequest request){
-        UserService.logout(request);
+        userService.logout(request);
     }
 }
