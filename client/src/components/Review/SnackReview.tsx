1d0e2cc3045969d7736845048ca8f3fe403dcce8
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2022-11-21 15:16:34 +0900

    feat: 리뷰리스트 별점기능 구현

--- client/src/components/Review/SnackReview.tsx
+++ client/src/components/Review/SnackReview.tsx
@@ -1,19 +1,20 @@
-import { BsStar, BsStarHalf, BsStarFill } from 'react-icons/bs';
+import { useState } from 'react';
+import { Rating } from 'react-simple-star-rating';
 
 const SnackReview = () => {
   return (
-    <div className="border w-[300px] h-[300px] p-4 rounded-3xl ">
-      <div className="flex">
+    <div className="border w-[300px] h-[300px] p-4 rounded-3xl shadow-md">
+      <div className="flex items-center justify-between">
         <img src="" alt="" className="w-16 h-16 rounded-full bg-slate-200" />
-        <div>username</div>
-        <div>date</div>
+        <div className="">
+          <div>username</div>
+          <div className="ml-auto">date</div>
+        </div>
       </div>
       <div className="grid grid-cols-2 my-1.5">
         <div className="flex">
           <div>디자인</div>
-          <div>
-            <BsStar />
-          </div>
+          <div></div>
         </div>
         <div className="flex">
           <div>품질</div>
@@ -28,7 +29,11 @@ const SnackReview = () => {
           <div></div>
         </div>
       </div>
-      <div className="text-justify">content</div>
+      <div className="text-justify h-[125px]">content</div>
+      <div className="flex items-end justify-end text-sm">
+        <button className="border m-0.5 p-0.5 rounded-lg">수정</button>
+        <button className="border m-0.5 p-0.5 rounded-lg">삭제</button>
+      </div>
     </div>
   );
 };
