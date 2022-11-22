0627b5d1efdac3ed13baf22737de5d80e1abef94
toneofrain <toneofrain@gmail.com>
2022-11-22 17:20:24 +0900

    feat: redis에 제품 별점 저장하도록 구현 (#111)

---
+++ server/src/main/java/seb/project/Codetech/redis/repository/ProductStatRepository.java
@@ -0,0 +1,8 @@
+package seb.project.Codetech.redis.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import seb.project.Codetech.redis.entity.ProductStat;
+
+public interface ProductStatRepository extends CrudRepository<ProductStat, Long> {
+}
