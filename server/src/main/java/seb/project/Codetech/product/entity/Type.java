c7596e06a4e00e478013f578aaad2766becf6244
NewWorld_ <mf_cheonwol@naver.com>
2022-11-29 19:45:13 +0900

    fix: 일부 오류 문법 수정 (#212)

--- server/src/main/java/seb/project/Codetech/product/entity/Type.java
+++ server/src/main/java/seb/project/Codetech/product/entity/Type.java
@@ -3,19 +3,13 @@ package seb.project.Codetech.product.entity;
 import seb.project.Codetech.global.converter.TypeValue;
 
 public enum Type implements TypeValue {
+
+	DESKTOP("데스크탑"),
+	LAPTOP("노트북"),
 	MONITOR("모니터"),
-	MOUSE("마우스"),
-	MICE("마이크"),
 	KEYBOARD("키보드"),
-	SPEAKER("스피커"),
-	NOTEBOOK("노트북"),
-	GAMEPAD("게임패드"),
-	TABLET("타플랫"),
-	MODEM("모뎀"),
-	HEADSET("해드셋"),
-	PHONE("핸드폰"),
-	LIGHTBAR("라이트바"),
-	WEBCAM("웹캠");
+	MOUSE("마우스"),
+	ETC("기타");
 
 	private final String value;
 
