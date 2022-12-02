b1119885c2a5bafd4d2d299e631068c241c025c4
Heera1 <caecus.regio@gmail.com>
2022-12-02 14:59:10 +0900

    fix: 리뷰 작성 페이지 수정완료 #262

--- client/src/pages/WriteReview.tsx
+++ client/src/pages/WriteReview.tsx
@@ -3,26 +3,20 @@
 
 import React, { ChangeEvent, useCallback, useState } from 'react';
 import '../components/common.css';
-import CategorieSelector from '../components/Selectors/CategorieSelector';
+import CategorySelector from '../components/Selectors/CategorySelector';
 import ProductSelector from '../components/Selectors/ProductSelector';
 import TextEditor from '../components/ToastUI/TextEditor';
 import useReview from '../store/review';
 import AddProduct from '../components/Modal/AddProduct';
-import { selectThumnailImg } from '../util/apiCollection';
+import ThumbnailModal from '../components/Modal/ThumbnailModal';
 
 const WriteReview = () => {
-  const {
-    title,
-    setTitle,
-    thumbnailBase64,
-    thumbnailImg,
-    setThumbnailBase64,
-    setThumnailImg,
-  } = useReview();
-  const [isModal, setIsModal] = useState(false);
+  const { title, setTitle, thumbnailImg } = useReview();
 
-  //썸네일 이미지 (프리뷰) 데이터 들어온 이미지 주소 저장
-  const [thumbnailCheck, setThumbnailCheck] = useState('/img');
+  //제품 추가 모달 열고 닫기 이벤트
+  const [isModal, setIsModal] = useState(false);
+  //썸네일 추가 모달 열고 닫기 이벤트
+  const [openThum, setOpenThum] = useState(false);
 
   //타이틀
   const onChangeTitle = useCallback((e: ChangeEvent<HTMLInputElement>) => {
@@ -30,100 +24,66 @@ const WriteReview = () => {
     setTitle(titleUpdate);
   }, []);
 
-  //모달
+  //제품추가 모달 이벤트 함수
   const onClickModal = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
     setIsModal(!isModal);
   };
 
-  //썸네일 이미지 저장
-  const handleChangeImg = async (e: any) => {
-    const reader = new FileReader();
-
-    if (e.target.files[0]) {
-      reader.readAsDataURL(e.target.files[0]);
-    }
-    reader.onloadend = () => {
-      const thumbnail: any = reader.result;
-      setThumbnailCheck(thumbnail);
-      setThumbnailBase64(e.target.files[0]);
-    };
-  };
-
-  const handelSubmitThumbnail = async (e: any) => {
-    const sizeData = {
-      width: 400,
-      height: 400,
-    };
-
+  //썸네일 모달 이벤트 함수
+  const onClickThumModal = (e: React.MouseEvent<HTMLElement>) => {
     e.preventDefault();
-    const formData = new FormData();
-    formData.append('file', thumbnailBase64);
-    formData.append(
-      'request',
-      new Blob([JSON.stringify(sizeData)] as any, { type: 'application/json' })
-    );
-
-    // api 요청
-    try {
-      const thumbnailPostApi = await selectThumnailImg(formData);
-      console.log(`thumbnailPostApi`, thumbnailPostApi.data);
-      setThumnailImg(thumbnailPostApi.data);
-      alert('업로드 완료!');
-    } catch (err: unknown) {
-      console.log(err);
-    }
+    setOpenThum(!openThum);
   };
 
   return (
-    <div className="flex flex-col items-center justify-center w-full bg-slate-300 max-md:pt-0">
-      <div className="bg-white lg:w-[64rem] pb-14 px-10">
-        {isModal == false ? null : (
+    <div className="flex flex-col items-center justify-center w-full bg-zinc-100 max-md:pt-0">
+      <div className="bg-white lg:w-[64rem] mx-auto w-full pb-14 px-10">
+        {isModal === false ? null : (
           <AddProduct isModal={isModal} setIsModal={setIsModal} />
         )}
+        {openThum === false ? null : (
+          <ThumbnailModal openThum={openThum} setOpenThum={setOpenThum} />
+        )}
         <div className="m-auto mt-8">
-          <div className="flex justify-between mb-3">
-            <div className="flex w-2/3 ">
+          <div className="flex justify-between h-12 mb-3">
+            <div className="flex w-4/5">
               <div className="inline-flex w-1/3 ">
-                <CategorieSelector />
+                <CategorySelector />
               </div>
               <div className="inline-flex w-1/3 ml-5 ">
                 <ProductSelector />
               </div>
             </div>
             <button
-              className="w-1/5 ml-20 so-button-nomal"
+              className="w-1/6 pb-1 mb-1 text-sm font-bold text-white rounded-md hover:bg-slate-400 bg-slate-300"
               onClick={onClickModal}
             >
               제품 추가하기
             </button>
           </div>
-          <div className="flex">
+          <div className="flex justify-between h-12 mb-5">
             <input
               type="text"
               id="title"
               value={title}
               onChange={onChangeTitle}
               placeholder="제목을 입력하세요"
-              className="w-4/5 mb-5 border signup-input border-slate-300"
+              maxLength={50}
+              className="w-4/5 mb-1 border signup-input border-slate-300"
             />
-            <form encType="multipart/form-data">
-              <input
-                type="file"
-                id="thumnail"
-                accept="image/*"
-                className="hidden"
-                onChange={handleChangeImg}
-              />
-              <label
-                role="button"
-                htmlFor="thumnail"
-                className="text-sm text-gray-500 p-[10px] rounded-2xl bg-white"
-              >
-                썸네일 선택하기
-              </label>
-              <button onClick={handelSubmitThumbnail}> 업로드</button>
-            </form>
+            <button
+              onClick={onClickThumModal}
+              className={
+                thumbnailImg.length === 0
+                  ? `w-1/6 pb-1 mb-1 text-sm font-bold text-white rounded-md hover:bg-slate-400 bg-slate-300`
+                  : `w-1/6 pb-1 mb-1 text-sm font-bold text-white bg-blue-600 rounded-md hover:bg-blue-500`
+              }
+            >
+              {thumbnailImg.length === 0
+                ? `썸네일 선택하기`
+                : `썸네일 수정하기`}
+            </button>
           </div>
           <div>
             <TextEditor />
