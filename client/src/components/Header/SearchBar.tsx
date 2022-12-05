ce7c697270c75c2258d4bce6a7a599f2015c1583
ethanJcoding <ethan.95j@gmail.com>
2022-12-05 17:22:57 +0900

    fix: 반응형 헤더 서치필드 수정

--- client/src/components/Header/SearchBar.tsx
+++ client/src/components/Header/SearchBar.tsx
@@ -1,9 +1,10 @@
-import { useState, KeyboardEvent } from 'react';
+import { useState, KeyboardEvent, useRef, useEffect } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
 import { searchBarProps } from '../../types/mainPageTypes';
 export default function SearchBar({ searchBar, setSearchBar }: searchBarProps) {
   const navigate = useNavigate();
+  const inputRef = useRef<HTMLInputElement>(null);
   const [keywords, setKeywords] = useState('');
   const onKeyPress = (e: KeyboardEvent) => {
     if (e.key == 'Enter') {
@@ -17,15 +18,20 @@ export default function SearchBar({ searchBar, setSearchBar }: searchBarProps) {
     }
   };
 
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [searchBar]);
+
   return (
     <>
       <form
         className={` ${
           searchBar ? null : 'max-sm:hidden'
-        } hover:bg-slate-100 flex items-center px-3 py-1 rounded-full w-[24rem] ring ring-inset ring-gray-100 hover:ring-0`}
+        } transition-all hover:bg-slate-100 flex items-center px-3 py-1 rounded-full w-[36rem] ring ring-inset ring-gray-100 hover:ring-0`}
       >
         <BiSearch className="text-3xl mr-2" />
         <input
+          ref={inputRef}
           type="text"
           className="w-full h-10 border-gray-300 bg-transparent ring-none outline-none font-medium"
           value={keywords}
@@ -33,6 +39,7 @@ export default function SearchBar({ searchBar, setSearchBar }: searchBarProps) {
           onChange={(e) => {
             setKeywords(e.target.value);
           }}
+          onBlur={() => setSearchBar(false)}
           onKeyPress={onKeyPress}
         />
       </form>
