ed32222ca5157d3a5a891d3af04e403db8021755
ethanJcoding <ethan.95j@gmail.com>
2022-12-07 09:24:31 +0900

    fix: 질문 답변 채택 에러 수정

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -5,20 +5,16 @@ const LandingPage = () => {
 
   return (
     <>
-      <div className="relative flex flex-col items-center justify-start py-40 bg-zinc-100 dark:bg-DMMainColor px-26 ">
-        <div className="flex flex-col items-center">
-          <div className="text-5xl font-medium sm:text-6xl  tracking-tighter mb-1 sm:mb-3 dark:text-white text-black/70">
-            관심있는 장비!
-          </div>
-          <div className="text-5xl font-medium sm:text-6xl  tracking-tighter mb-1 sm:mb-3 dark:text-white text-black/70">
-            사고싶은 장비!
-          </div>
-          <div className="font-bold text-5xl sm:text-7xl lg:text-8xl pl-1 text-blue-600 mb-1 sm:mb-3 tracking-tighter">
-            자세한 리뷰가
-          </div>
-          <div className="flex pr-1 text-5xl font-semibold sm:text-6xl lg:text-7xl tracking-tighter dark:text-white text-black">
-            궁금하다면?
-          </div>
+      <div className="flex flex-col items-center justify-start py-10 bg-white px-26 dark:bg-DMMainColor">
+        <div className="flex flex-col items-center lg:mt-20">
+          <p className="text-2xl font-medium lg:text-6xl sm:text-3xl md:text-4xl">
+            관심있는 장비! 사고싶은 장비!
+          </p>
+          <br />
+          <p className="flex pr-1 text-3xl font-semibold lg:text-7xl sm:text-4xl md:text-5xl">
+            <p className="pl-1 mr-3 text-blue-600 lg:mr-5">자세한 리뷰가 </p>
+            {` `}궁금하다면?
+          </p>
           <button
             className=" pt-4 pb-5 tracking-tight text-3xl sm:text-6xl px-12 bg-blue-600 rounded-full text-white mt-12 text-center md:text-4xl md:mb-8 lg:pt-7 lg:pb-8 lg:px-16 font-medium\"
             onClick={() => navigate('/')}
